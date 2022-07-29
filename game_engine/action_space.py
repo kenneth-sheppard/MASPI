8e6627699af7d004e30c072e48199e25c0dc7864
Kenneth Sheppard <kenneth.sheppard.18@cnu.edu>
2022-07-28 21:32:41 -0400

    Create file to hold the actions of the rondel, and the parent class ActionSpace

---
+++ game_engine/action_space.py
@@ -0,0 +1,45 @@
+
+
+class ActionSpace:
+    def __init__(self):
+        self.name = None
+        self.next_action = None
+
+    def get_name(self):
+        return self.name
+
+    def get_next_action(self):
+        return self.next_action
+
+    def set_next_action(self, na):
+        self.next_action = na
+
+
+class Investor(ActionSpace):
+    def __init__(self):
+        pass
+
+
+class Import(ActionSpace):
+    def __init__(self):
+        pass
+
+
+class Production(ActionSpace):
+    def __init__(self):
+        pass
+
+
+class Maneuver(ActionSpace):
+    def __init__(self):
+        pass
+
+
+class Taxation(ActionSpace):
+    def __init__(self):
+        pass
+
+
+class Factory(ActionSpace):
+    def __init__(self):
+        pass
