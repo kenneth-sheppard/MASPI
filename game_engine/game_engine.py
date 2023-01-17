04288334e9ccdd8b055f28d9231acab3f65ed44f
Kenneth Sheppard <kenneth.sheppard.18@cnu.edu>
2023-01-16 20:18:23 -0500

    Check so players cannot overspend while moving around rondel

--- game_engine/game_engine.py
+++ game_engine/game_engine.py
@@ -26,7 +26,7 @@ class GameEngine:
                 self.state
             )[0]
             # Tax the player if moving too far
-            self.__move_tax(ntm)
+            ntm = self.__move_query()
             # Advance that many spaces on the rondel
             self.active_country.advance(ntm)
             # Activate that action space on the rondel
@@ -36,6 +36,11 @@ class GameEngine:
 
         for country in self.state.get_countries():
             print(f'{country.get_name()} - {country.get_power()}')
+
+        space = self.active_country.get_rondel_space()
+        for i in range(0, 8):
+            print(f'{space.get_name()} - {space.get_action().get_times_activated()} times')
+            space = space.next()
 
     def __next_active_country(self):
         if self.active_country is None or self.active_country.get_name() == 'European Union':
@@ -51,10 +56,18 @@ class GameEngine:
         elif self.active_country.get_name() == 'America':
             self.active_country = self.state.get_country('European Union')
 
-    def __move_tax(self, ntm):
-        # TODO cannot go negative in money
+    def __move_query(self):
+        options = []
+
+        for i in range(0, 6):
+            if self.active_player.get_money() - (i - 3) * \
+                    (1 + helper.power_chart(self.active_country.get_power())) >= 0:
+                options.append([i, self.active_country.hypothetical_advance(i).get_name()])
+
+        ntm = self.active_player.make_rondel_choice(options, self.state)[0]
+
         if ntm > 3:
             self.active_player.remove_money((ntm - 3) * (1 + helper.power_chart(self.active_country.get_power())))
 
-        return
+        return ntm
         
