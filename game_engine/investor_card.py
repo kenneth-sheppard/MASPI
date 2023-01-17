e49b2ca7145a0364ec065d7944eb8c85494ebf44
Kenneth Sheppard <kenneth.sheppard.18@cnu.edu>
2023-01-16 20:47:24 -0500

    Track players in the order to be passed around by the investor card

--- game_engine/investor_card.py
+++ game_engine/investor_card.py
@@ -9,3 +9,35 @@ class InvestorCard:
 
     def set_controller(self, c):
         self.controller = c
+
+    def add_player(self, p):
+        # if no controller then this player is temporarily the controller
+        if self.controller is None:
+            self.controller = self.ICPlayer(p)
+        # Otherwise add new players as the next player
+        else:
+            new_player = self.controller
+            while new_player.get_next() is not None:
+                new_player = new_player.get_next()
+
+            new_player.set_next(self.ICPlayer(p))
+
+    def done_adding_players(self):
+        # now that done adding player move control to the Russia controller
+        while 'Russia' not in self.controller.get_player().get_controlled_countries():
+            print('Next')
+            self.controller = self.controller.get_next()
+
+    class ICPlayer:
+        def __init__(self, player):
+            self.player = player
+            self.next_player = None
+
+        def set_next(self, next_player):
+            self.next_player = next_player
+
+        def get_next(self):
+            return self.next_player
+
+        def get_player(self):
+            return self.player
