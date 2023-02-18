3a2395b2d63ab9ff0345c1154bbdfa6d2d7ff364
Kenneth Sheppard <kenneth.sheppard.18@cnu.edu>
2023-02-17 23:49:49 -0500

    Keep track of the type of player

--- statistics_observer/player_observer.py
+++ statistics_observer/player_observer.py
@@ -8,19 +8,23 @@ class PlayerObserver:
         self.player_info = {
             'bonds': p.get_bonds(),
             'money': p.get_worth(),
-            'controlled_countries': p.get_controlled_countries()
+            'controlled_countries': p.get_controlled_countries(),
+            'type': p.get_type(),
+            'scores': []
         }
         self.wins = 0
         self.game_log = []
         self.id = random.randint(0, 5000)
 
     def __str__(self):
-        return f'Player {self.id} won {self.wins} games'
+        return f'Player {self.id} {self.player_info["type"]} won {self.wins} games with scores of {self.player_info["scores"]}'
         
     def observe(self):
         self.player_info['bonds'] = self.player.get_bonds()
         self.player_info['money'] = self.player.get_worth()
         self.player_info['controlled_countries'] = self.player.get_controlled_countries()
+        self.player_info['scores'].append(self.player.get_worth())
+        self.player_info['type'] = self.player.get_type()
         
     def was_winner(self):
         self.wins += 1
