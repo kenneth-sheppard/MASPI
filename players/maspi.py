7b006c2e022f02b3fa7745aead392721a1905f2a
Bruce <kenneth.sheppard.18@cun.edu>
2023-06-09 19:13:34 -0400

    Split existing players into individual files

--- players/maspi.py
+++ players/maspi.py
@@ -54,7 +54,7 @@ class PlayerMASPI(Player):
         best_option = None
         best_value = None
         for option in options:
-            new_engine = game_engine.game_engine.potential_advance(option, copy.deepcopy(engine_game))
+            new_engine = game_engine.potential_advance(option, copy.deepcopy(engine_game))
             new_eval = self.__evaluate_game_state(new_engine.state)
             if best_option is None or new_eval > best_value:
                 best_value = new_eval
@@ -85,3 +85,180 @@ class PlayerMASPI(Player):
                 best_option = option
 
         return best_option
+
+
+class MASPIPart:
+
+    def __init__(self):
+        self.name = 'Basic Part'
+
+    def pass_down(self):
+        pass
+
+    def pass_up(self):
+        pass
+
+    def receive_up(self):
+        pass
+
+    def receive_down(self):
+        pass
+
+
+class TerritoryAgent(MASPIPart):
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'Territory Agent'
+
+    def pass_up(self):
+        # Get all adjacent territories
+        # For each adjacent territory query value
+        # Calculate own value
+        # Identify the best location for each unit in territory
+        # Probably best to loop for number of units present times
+        # Return unit plans
+        pass
+
+    def receive_down(self):
+        # Expected from above
+        # Aggressiveness
+        # Carelessness
+        pass
+
+
+class ManeuverCollector(MASPIPart):
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'Maneuver Collector'
+
+    def pass_down(self):
+        # Pass down
+        # Aggressiveness
+        # Carelessness
+        pass
+
+    def pass_up(self):
+        # Pass that to the rondel manager maybe?
+        pass
+
+    def receive_down(self):
+        # Expected from above
+        # Violence
+        # Expansiveness
+        pass
+
+    def receive_up(self):
+        # Expected from below
+        # A bunch of territory agent evaluations
+        # Select the best priorities
+        pass
+
+
+class RondelManager(MASPIPart):
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'Rondel Manager'
+
+    def pass_down(self):
+        # Pass Down
+        pass
+
+    def pass_up(self):
+        # I don't think anything needs to be passed up
+        pass
+
+    def receive_down(self):
+        # Expected from above
+        # Make money
+        # Build force
+        # Expand Territory
+        pass
+
+    def receive_up(self):
+        # Expected from below
+        # The way to perform the maneuver action
+        pass
+
+    # Spaces on the Rondel
+    # Maneuver - Expand territory
+    # Production - Build force
+    # Import - Build force
+    # Taxation - Make money
+    # Investor - Make money
+    # Factory - Build force
+    def select_space(self):
+        pass
+
+
+class MilitaryManager(MASPIPart):
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'Military Manager'
+
+    def pass_down(self):
+        # Pass Down
+        # Violence
+        # Expansiveness
+        pass
+
+    def pass_up(self):
+        # I don't think anything needs to be passed up
+        pass
+
+    def receive_down(self):
+        # Expected from above
+        pass
+
+    def receive_up(self):
+        # Expected from below
+        pass
+
+
+class InvestorManager(MASPIPart):
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'Investor Manager'
+
+    def pass_down(self):
+        # Pass Down
+        pass
+
+    def pass_up(self):
+        # Pass Up
+        pass
+
+    def receive_down(self):
+        # Expected from above
+        pass
+
+    def receive_up(self):
+        # Expected from below
+        pass
+
+
+class CountryEvaluator(MASPIPart):
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'Country Evaluator'
+
+    def pass_down(self):
+        # Pass Down
+        pass
+
+    def pass_up(self):
+        # Pass Up
+        pass
+
+    def receive_down(self):
+        # Expected from above
+        pass
+
+    def receive_up(self):
+        # Expected from below
+        pass
