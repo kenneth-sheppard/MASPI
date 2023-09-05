8cf9ae04bf9da1666066af732da8c6db22dec5d8
Bruce <kenneth.sheppard.18@cun.edu>
2023-09-04 21:00:17 -0400

    basic_nn: use random selection for more complex actions

--- players/basic_nn.py
+++ players/basic_nn.py
@@ -1,4 +1,6 @@
 import copy
+import random
+
 import tensorflow as tf
 
 import game_engine.action_space as action_space
@@ -17,33 +19,35 @@ class BasicNeuralNetPlayer(Player):
     def __evaluate_game_state(self, game_state):
         state = game_state.get_numerical_representation()
         reshaped_state = tf.reshape(state, (1, 1302))
-        values = self.model.predict(reshaped_state)[0]
+        values = self.model.predict(reshaped_state, verbose=0)[0]
 
         return values[self.id]
 
     def make_import_choice(self, options, game_state):
-        best_option = None
-        best_value = None
-        for option in options:
-            new_state = action_space.hypothetical_import(option, copy.deepcopy(game_state))
-            new_eval = self.__evaluate_game_state(new_state)
-            if best_option is None or new_eval > best_value:
-                best_value = new_eval
-                best_option = option
-
-        return best_option
+        # best_option = None
+        # best_value = None
+        # for option in options:
+        #     new_state = action_space.hypothetical_import(option, copy.deepcopy(game_state))
+        #     new_eval = self.__evaluate_game_state(new_state)
+        #     if best_option is None or new_eval > best_value:
+        #         best_value = new_eval
+        #         best_option = option
+        #
+        # return best_option
+        return options[int(random.random() * len(options))]
 
     def make_maneuver_choice(self, options, game_state):
-        best_option = None
-        best_value = None
-        for option in options:
-            new_state = action_space.hypothetical_move_piece(option, copy.deepcopy(game_state))
-            new_eval = self.__evaluate_game_state(new_state)
-            if best_option is None or new_eval > best_value:
-                best_value = new_eval
-                best_option = option
-
-        return best_option
+        # best_option = None
+        # best_value = None
+        # for option in options:
+        #     new_state = action_space.hypothetical_move_piece(option, copy.deepcopy(game_state))
+        #     new_eval = self.__evaluate_game_state(new_state)
+        #     if best_option is None or new_eval > best_value:
+        #         best_value = new_eval
+        #         best_option = option
+        #
+        # return best_option
+        return options[int(random.random() * len(options))]
 
     def make_battle_choice(self, options, game_state):
         best_option = None
